b510584114aeaf0a26a27bd9d1439d3acd06a35a
AlanJhonatan <afranca2@lenovo.com>
2022-02-09 12:32:27 -0300

    chore: added product create with connect category

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController';
+import { CreateProductCategory } from './controllers/CreateProductCategoryController';
 import { CreateProductController } from './controllers/CreateProductController';
+import { CreateProductWithExistCategory } from './controllers/CreateProductWithExistCategory';
 
 const router = Router();
 
 const createProduct = new CreateProductController();
 const createCategory = new CreateCategoryController();
+const createproductCategory = new CreateProductCategory();
+const createProductWithExistCategory = new CreateProductWithExistCategory();
 
 router.post('/product', createProduct.handle);
 router.post('/category', createCategory.handle);
+router.post('/productCategory', createproductCategory.handle);
+router.post('/productWithCategory', createProductWithExistCategory.handle);
 
 export { router };
